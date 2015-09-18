1016e4697b43304e594146aab9cb5d6b7eb1b329
jnf <jeremy.flores@gmail.com>
2015-09-17 20:25:37 -0700

    Flesh out the movie model capabilities and tests, but I've got a weird race condition to figure out.

--- models/movie.js
+++ models/movie.js
@@ -14,6 +14,52 @@ Movie.prototype = {
     this.db.all(statement, title, function(err, res) {
       if (callback) callback(err, res);
     });
+
+    this.db.close();
+  },
+
+  create: function(data, callback) {
+    var keys = Object.keys(data);
+    var key_pairs = [];
+    var values = [];
+
+    for (var i = 0; i < keys.length; i++) {
+      values.push(data[keys[i]]);
+      key_pairs.push('?');
+    }
+
+    var statement = "INSERT INTO movies(" + keys.join(',') + ") " +
+                    "VALUES(" + key_pairs.join(',') + ")"
+
+    this.db.run(statement, values, function(err) {
+        callback(err, { inserted_id: this.lastID, changed: this.changes });
+    });
+
+    this.db.close();
+  },
+
+  save: function(data, callback) {
+    if (data.id) {
+      var keys = Object.keys(data);
+      var key_pairs = [];
+      var values = [];
+
+      for (var i = 0; i < keys.length; i++) {
+        values.push(data[keys[i]]);
+        key_pairs.push(keys[i] + "=? ");
+      }
+
+      var statement = "UPDATE movies SET " + key_pairs.join(',') + "WHERE id=" + data.id;
+      
+      this.db.run(statement, values, function(err) {
+          callback(err, { inserted_id: this.lastID, changed: this.changes });
+      });
+      
+      this.db.close();
+
+    } else {
+      callback({err: "Missing Key", message: "Can't save without an id; use `create`"});
+    }
   }
 }
 
--- test/models/movie.js
+++ test/models/movie.js
@@ -3,11 +3,27 @@ var assert = require('assert'),
     sqlite3 = require('sqlite3').verbose();
 
 
-describe("Movie", function() {
-  var movie;
 
-  beforeEach(function() {
+describe("Movie", function() {
+  var movie, db_cleaner
+
+  beforeEach(function(done) {
     movie = new Movie();
+
+    db_cleaner = new sqlite3.Database('db/test.db');
+    db_cleaner.serialize(function() {
+      db_cleaner.exec(
+        "BEGIN; \
+        DELETE FROM movies; \
+        INSERT INTO movies(title, overview, release_date, inventory) \
+        VALUES('Jaws', 'Shark!', 'Yesterday', 10); \
+        COMMIT;"
+        , function(err) {
+          db_cleaner.close();
+          done();
+        }
+      );
+    });
   })
 
   it("can be instantiated", function() {
@@ -29,6 +45,40 @@ describe("Movie", function() {
         done();
       })
     })
+
+    it("can save changes to a movie", function(done) {
+      movie.find("Jaws", function(err, res) {
+        var original_title = res[0].title;
+        var id = res[0].id;
+
+        movie.save({title: "Jaws 2: Jawsier", id: id}, function(err, res) {
+          assert.equal(err, undefined);
+          assert.equal(res.inserted_id, 0); //it didn't insert any records
+          assert.equal(res.changed, 1); //it updated one record
+          done();
+        })
+      })
+    });
+
+    it("can save a new movie to the database", function(done) {
+      var data = {
+        title: "RoboJaws",
+        overview: "Jaws is hunted by RoboJaws",
+        release_date: "Tomorrow",
+        inventory: 10
+      }
+
+      movie.create(data, function(err, res) {
+        assert.equal(res.inserted_id, 2); //it inserted a new record
+        assert.equal(res.changed, 1); //one record was changed
+
+        movie.find("RoboJaws", function(err, res) {
+          assert.equal(res.length, 1);
+          assert.equal(res[0].title, 'RoboJaws'); //we found our new movie
+          done();
+        })
+      })
+    });
   })
 })
 
