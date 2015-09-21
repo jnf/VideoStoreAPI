b5f89eeff5d5e3b4be74c7a3cf12da80089ad81f
jnf <jeremy.flores@gmail.com>
2015-09-20 22:45:52 -0700

    Add my next failing test: /movies should return json movie data in a specific format.

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.status(200)
-     .json({});
+     .json([{why: 'woo'}]);
 });
 
 module.exports = router;
--- test/endpoints/movies.js
+++ test/endpoints/movies.js
@@ -1,14 +1,48 @@
 var request = require('supertest'),
+    assert  = require('assert'),
     app     = require('../../app'),
+    sqlite3 = require('sqlite3').verbose(),
     agent   = request.agent(app);
 
 describe("Endpoints under /movies", function() {
   describe("GET /", function() {
+    var movie_request;
+    
+    beforeEach(function(done) {
+      movie_request = request(app).get('/movies').set('Accept', 'application/json');
+
+      db_cleaner = new sqlite3.Database('db/test.db');
+      db_cleaner.serialize(function() {
+        db_cleaner.exec(
+          "BEGIN; \
+          DELETE FROM movies; \
+          INSERT INTO movies(title, overview, release_date, inventory) \
+          VALUES('Jaws', 'Shark!', 'Yesterday', 10), \
+                ('Maws', 'Worm!', 'Yesterday', 11); \
+          COMMIT;"
+          , function(err) {
+            db_cleaner.close();
+            done();
+          }
+        );
+      });
+    })
+
     it("responds with json", function(done) {
-      request(app)
-        .get('/movies').set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
+      movie_request
+        .expect('Content-Type', /application\/json/)
         .expect(200, done);
+    })
+
+    it("returns an array of movie objects", function(done) {
+      movie_request.expect(200, function(error, result) {
+        console.log(result.body);
+        assert.equal(result.body.length, 2); //the db_cleaner inserted two records
+
+        var keys = ['title', 'overview', 'release_date', 'inventory'];
+        assert.equal(Object.keys(result.body[0]), keys);
+        done();
+      })
     })
   })
 })
